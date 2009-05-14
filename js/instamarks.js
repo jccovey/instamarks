c19e675ad6a9bc9995de2680ef69dbc5afed7e55
John Covey <john@john-coveys-computer.local>
2009-05-14 01:19:31 -0500

    Wrapped bookmarklet in closure to prevent naming collisions

--- js/instamarks.js
+++ js/instamarks.js
@@ -2,160 +2,168 @@
 javascript:function%20loadScript(scriptURL)%20{%20var%20scriptElem%20=%20document.createElement('SCRIPT');%20scriptElem.setAttribute('language',%20'JavaScript');%20scriptElem.setAttribute('src',%20scriptURL);%20document.body.appendChild(scriptElem);}%20loadScript('http://fsavard.com/code/landmarks.js');
 */
 
-function loadCss(url) {
-    var headID = document.getElementsByTagName("head")[0];
-    var cssNode = document.createElement('link');
-    cssNode.type = 'text/css';
-    cssNode.rel = 'stylesheet';
-    cssNode.href = url;
-    cssNode.media = 'screen';
-    headID.appendChild(cssNode);
-}
+(function () {
 
-// Taken from http://stackoverflow.com/questions/756382/bookmarklet-wait-until-javascript-is-loaded
-function loadScript(url, callback)
-{
-    var head = document.getElementsByTagName("head")[0];
-    var script = document.createElement("script");
-    script.src = url;
+	var scripts = [
+		'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js',
+	 	'http://jqueryui.com/latest/ui/ui.core.js',
+		'http://jqueryui.com/latest/ui/ui.draggable.js',
+		'http://instamarks.appspot.com/js/uuid.js'
+	];
+	
+	function loadCss(url) {
+	    var headID = document.getElementsByTagName("head")[0];
+	    var cssNode = document.createElement('link');
+	    cssNode.type = 'text/css';
+	    cssNode.rel = 'stylesheet';
+	    cssNode.href = url;
+	    cssNode.media = 'screen';
+	    headID.appendChild(cssNode);
+	}
+	
+	// Taken from http://stackoverflow.com/questions/756382/bookmarklet-wait-until-javascript-is-loaded
+	function loadScript(url, callback)
+	{
+	    var head = document.getElementsByTagName("head")[0];
+	    var script = document.createElement("script");
+	    script.src = url;
 
-    // Attach handlers for all browsers
-    var done = false;
-    script.onload = script.onreadystatechange = function()
-    {
-        if( !done && ( !this.readyState
-            || this.readyState == "loaded"
-            || this.readyState == "complete") )
-            {
-            done = true;
+	    // Attach handlers for all browsers
+	    var done = false;
+	    script.onload = script.onreadystatechange = function()
+	    {
+	        if( !done && ( !this.readyState
+	            || this.readyState == "loaded"
+	            || this.readyState == "complete") )
+	            {
+	            done = true;
 
-            // Continue your code
-            callback();
+	            // Continue your code
+	            callback();
 
-            // Handle memory leak in IE
-            script.onload = script.onreadystatechange = null;
-            head.removeChild( script );
-        }
-    };
+	            // Handle memory leak in IE
+	            script.onload = script.onreadystatechange = null;
+	            head.removeChild( script );
+	        }
+	    };
 
-    head.appendChild(script);
-}
+	    head.appendChild(script);
+	}
 
-var scriptUrls = [
-	'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js',
- 	'http://jqueryui.com/latest/ui/ui.core.js',
-	'http://jqueryui.com/latest/ui/ui.draggable.js',
-	'/js/uuid.js'
-];
+	function loadScripts(lastCallback, nextId){
+	    if(!nextId) nextId = 0;
+	    if(nextId >= scripts.length){
+			loadCss('http://instamarks.appspot.com/css/instamarks.css');
+	        lastCallback(); return;
+	    }
+	    loadScript(scripts[nextId], function(){
+	        loadScripts(lastCallback, nextId+1);
+	    });
+	}
+	
+	// Taken from http://www.codetoad.com/javascript_get_selected_text.asp
+	function getSelText(){
+	    var txt = '';
+	    if (window.getSelection){
+	        return window.getSelection();
+	    }else if (document.getSelection){
+	        return document.getSelection();
+	    }else if (document.selection){
+	        return document.selection.createRange().text;
+	    }else
+	        return;
+	}
 
-function loadScripts(lastCallback, nextId){
-    if(!nextId) nextId = 0;
-    if(nextId >= scriptUrls.length){
-		loadCss('/css/instamarks.css');
-        lastCallback(); return;
-    }
-    loadScript(scriptUrls[nextId], function(){
-        loadScripts(lastCallback, nextId+1);
-    });
-}
+	// adapted from http://snipplr.com/view.php?codeview&id=10912
+	function getSelRange() {
+	    var userSelection;
+	    if (window.getSelection) {
+	        // W3C Ranges
+	        userSelection = window.getSelection ();
 
-// Taken from http://www.codetoad.com/javascript_get_selected_text.asp
-function getSelText(){
-    var txt = '';
-    if (window.getSelection){
-        return window.getSelection();
-    }else if (document.getSelection){
-        return document.getSelection();
-    }else if (document.selection){
-        return document.selection.createRange().text;
-    }else
-        return;
-}
+	        if(!userSelection) return '';
 
-// adapted from http://snipplr.com/view.php?codeview&id=10912
-function getSelRange() {
-    var userSelection;
-    if (window.getSelection) {
-        // W3C Ranges
-        userSelection = window.getSelection ();
+	        // Get the range:
+	        if (userSelection.getRangeAt)
+	            var range = userSelection.getRangeAt (0);
+	        else {
+	            var range = document.createRange ();
+	            range.setStart (userSelection.anchorNode, userSelection.anchorOffset);
+	            range.setEnd (userSelection.focusNode, userSelection.focusOffset);
+	        }
 
-        if(!userSelection) return '';
+	        return range;
+	    } else if (document.selection) {
+	        // Explorer selection, return the HTML
+	        return document.selection.createRange ();
+	    } else {
+	        return undefined;
+	    }
+	}
 
-        // Get the range:
-        if (userSelection.getRangeAt)
-            var range = userSelection.getRangeAt (0);
-        else {
-            var range = document.createRange ();
-            range.setStart (userSelection.anchorNode, userSelection.anchorOffset);
-            range.setEnd (userSelection.focusNode, userSelection.focusOffset);
-        }
+	// Adapted fom http://www.mirrors.docunext.com/cgi-bin/jquery-hhh333-plugins/view/trunk/jqueryRte/jquery.rte.js?rev=4
+	function insertNodeAtRangeStart(node,range){
+	    if(range.insertNode){ // normal way
+	        range.insertNode(node);
+	    }else{ // ugly ie hack
+	        var tmpParent = this.createElement('span');
+	        tmpParent.appendChild(node);
+	        r.collapse();
+	        r.pasteHTML(tmpParent.innerHTML);
+	    }
+	}
 
-        return range;
-    } else if (document.selection) {
-        // Explorer selection, return the HTML
-        return document.selection.createRange ();
-    } else {
-        return undefined;
-    }
-}
-
-// Adapted fom http://www.mirrors.docunext.com/cgi-bin/jquery-hhh333-plugins/view/trunk/jqueryRte/jquery.rte.js?rev=4
-function insertNodeAtRangeStart(node,range){
-    if(range.insertNode){ // normal way
-        range.insertNode(node);
-    }else{ // ugly ie hack
-        var tmpParent = this.createElement('span');
-        tmpParent.appendChild(node);
-        r.collapse();
-        r.pasteHTML(tmpParent.innerHTML);
-    }
-}
-
-function newLandmark(){
-    var range = getSelRange();
-    if(!range){
-        alert("Browser not supported");
-        return;
-    }
-    var title = ''+getSelText();
-    if(title.length == 0){
-        alert("You first need to select some text to serve as landmark.");
-        return;
-    }
-    if(title.length > 50){
-        title = title.substr(0,50) + '...';
-    }
-
-    var uuid = UUID.generate('v4');
-    var landmark = jQuery('<a/>').attr('name',uuid);
-    insertNodeAtRangeStart(landmark.get(0), range);
-
-    var listitem = jQuery('<li/>');
-    jQuery('div#instamarks ul').append(listitem);
-    var anchorlink = jQuery('<a href="#'+uuid+'">'+title+'</a>');
-    listitem.append(anchorlink);
-}
-
-function proceed() {
-	var container = jQuery('<div />');
-	var list = jQuery('<ul />')
+	function proceed() {
+		var $, container;
 		
-	container.attr('id', 'instamarks');
-	container.html('<div id="handle">Instamarks!</div> <button id="new">Add</button>');
-	container.append(list);
+		$ = jQuery.noConflict(true);
+		
+		function newLandmark(){
+		    var range = getSelRange();
+		    if(!range){
+		        alert("Browser not supported");
+		        return;
+		    }
+		    var title = ''+getSelText();
+		    if(title.length == 0){
+		        alert("You first need to select some text to serve as landmark.");
+		        return;
+		    }
+		    if(title.length > 50){
+		        title = title.substr(0,50) + '...';
+		    }
 
-	jQuery('body').append(container);
-	
-	jQuery('div#instamarks').hover(function() {
-		jQuery('div#instamarks ul').css('height', 'inherit');
-	}, function() {
-		jQuery('div#instamarks ul').css('height', '0');
-	});
-	
-	jQuery('div#instamarks button').click(function() {
-		newLandmark();
-	});
-	
-}
+		    var uuid = UUID.generate('v4');
+		    var landmark = $('<a/>').attr('name',uuid);
+		    insertNodeAtRangeStart(landmark.get(0), range);
 
-loadScripts(proceed, 0);
+		    var listitem = $('<li/>');
+		    $('div#instamarks ul').append(listitem);
+		    var anchorlink = $('<a href="#'+uuid+'">'+title+'</a>');
+		    listitem.append(anchorlink);
+		}
+		
+		container = $('<div />');
+		container.attr('id', 'instamarks');
+		container.html('<div id="handle">Instamarks!</div> <button id="new">Add</button>');
+		container.append($('<ul />'));
+
+		$('body').append(container);
+
+		$('div#instamarks div#handle').mouseenter(function() {
+			$('div#instamarks ul').css('height', 'inherit');
+		});
+		
+		$('div#instamarks').mouseleave(function() {
+			$('div#instamarks ul').css('height', '0');
+		});
+
+		$('div#instamarks button').click(function() {
+			newLandmark();
+		});
+
+	}
+
+	loadScripts(proceed, 0);
+	
+}());
